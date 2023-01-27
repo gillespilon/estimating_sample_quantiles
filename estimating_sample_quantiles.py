01fc7856f9b05879723c8acbf192447b88e8e7b1
Gilles Pilon <gilles.pilon@gillespilon.ca>
2023-01-27 14:54:28 -0600

    Capitalize constants, add html output

--- estimating_sample_quantiles.py
+++ estimating_sample_quantiles.py
@@ -9,16 +9,24 @@ import pandas as pd
 
 
 def main():
-    data = {"y": [0, 0, 1, 2, 63, 61, 27, 13]}
-    df = pd.DataFrame(data=data)
-    series_name_y = "y"
+    OUTPUT_URL = "estimating_sample_quantiles.html"
+    HEADER_TITLE = "Estimating Sample Quantiles"
+    DATA = {"y": [0, 0, 1, 2, 63, 61, 27, 13]}
+    HEADER_ID = "estimating-sample-quantiles"
+    PROB = (0.25, 0.50, 0.75)
+    SERIES_NAME_Y = "y"
+    original_stdout = ds.html_begin(
+        output_url=OUTPUT_URL,
+        header_title=HEADER_TITLE,
+        header_id=HEADER_ID
+    )
+    df = pd.DataFrame(data=DATA)
     print("pandas.Series.describe():")
-    print()
-    print(df[series_name_y].describe())
+    print(df[SERIES_NAME_Y].describe())
     print()
     method06 = ms.mquantiles(
-        df[series_name_y],
-        prob=(0.25, 0.50, 0.75),
+        df[SERIES_NAME_Y],
+        prob=PROB,
         alphap=0,
         betap=0
     )
@@ -26,8 +34,8 @@ def main():
     print(method06)
     print()
     method07 = ms.mquantiles(
-        df[series_name_y],
-        prob=(0.25, 0.50, 0.75),
+        df[SERIES_NAME_Y],
+        prob=PROB,
         alphap=1,
         betap=1
     )
@@ -37,8 +45,8 @@ def main():
     print(method07)
     print()
     method08 = ms.mquantiles(
-        df[series_name_y],
-        prob=(0.25, 0.50, 0.75),
+        df[SERIES_NAME_Y],
+        prob=PROB,
         alphap=1/3,
         betap=1/3
     )
@@ -46,8 +54,8 @@ def main():
     print(method08)
     print()
     method10 = ms.mquantiles(
-        df[series_name_y],
-        prob=(0.25, 0.50, 0.75),
+        df[SERIES_NAME_Y],
+        prob=PROB,
         alphap=.4,
         betap=.4
     )
@@ -55,9 +63,13 @@ def main():
     print(method10)
     print()
     print("ds.nonparametric_summary()")
-    series = ds.nonparametric_summary(series=df[series_name_y])
+    series = ds.nonparametric_summary(series=df[SERIES_NAME_Y])
     print(series)
     print()
+    ds.html_end(
+        original_stdout=original_stdout,
+        output_url=OUTPUT_URL
+    )
 
 
 if __name__ == "__main__":
